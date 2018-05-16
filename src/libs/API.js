731ad98b28aeb49c02589d0ac59dac1e6920bcfe
Beppe Karlsson <beppe@live.se>
2018-05-17 01:00:03 +0200

    API lib

---
+++ src/libs/API.js
@@ -0,0 +1,28 @@
+import API_URL from './API_URL';
+import axios from 'axios';
+
+export const post = (url, data) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${API_URL}/${url}`, data)
+      .then(res => {
+        resolve(res);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+export const customCall = (url, config) => {
+  return new Promise((resolve, reject) => {
+    config.url = `${API_URL}/${url}`;
+    axios(config)
+      .then(res => {
+        resolve(res);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
---
+++ src/libs/API_URL.js
@@ -0,0 +1,4 @@
+const LOCAL = 'http://localhost:8000';
+const API_URL = LOCAL;
+
+export default API_URL;
