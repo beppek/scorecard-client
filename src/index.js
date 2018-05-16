d9bfee20de24a7386f59cb7534e9a9dcc7a07ce3
Beppe Karlsson <beppe@live.se>
2018-05-17 00:59:10 +0200

    store provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './state/store/';
+import * as authActions from './state/actions/authActions';
+
+store.dispatch(dispatch => {
+  authActions.checkCookie(dispatch);
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
